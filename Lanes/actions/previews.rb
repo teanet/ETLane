56b54861035d526146c28d5364bc74d1574ad299
e.tyutyuev <e.tyutyuev@2gis.ru>
2020-04-14 21:11:26 +0700

    Better app preview upload

--- Lanes/actions/previews.rb
+++ Lanes/actions/previews.rb
@@ -1,20 +1,19 @@
 require 'credentials_manager/account_manager'
 require 'fastlane_core/itunes_transporter'
+require 'rexml/document'
+        # a = CredentialsManager::AccountManager.new(user: user, prefix: "deliver.appspecific", note: "application-specific")
+        # @password = a.password(ask_if_missing: true) # to ask the user for the missing value
 
 module Fastlane
   module Actions
     class PreviewsAction < Action
       def self.run(params)
-        require 'rexml/document'
-
         user = params[:username]
         product_bundle_identifier = CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
         spaceship = Spaceship::Tunes.login(user)
         spaceship.team_id = CredentialsManager::AppfileConfig.try_fetch_value(:itc_team_id)
         app = Spaceship::Tunes::Application.find(product_bundle_identifier)
         app_id = app.apple_id()
-        # a = CredentialsManager::AccountManager.new(user: user, prefix: "deliver.appspecific", note: "application-specific")
-        # @password = a.password(ask_if_missing: true) # to ask the user for the missing value
         transporter = FastlaneCore::ItunesTransporter.new(user)
         destination = "/tmp"
         itmsp_path = File.join(destination, "#{app_id}.itmsp")
@@ -28,21 +27,27 @@ module Fastlane
         doc = REXML::Document.new(File.read(metadata_path))
         current_version = doc.root.elements["software/software_metadata/versions[1]/version"]
 
-        old_version = doc.root.elements["software/software_metadata/versions"]
-        old_version.elements.delete(old_version.elements[2])
+        versions = doc.root.elements["software/software_metadata/versions"]
+        old_version = versions.elements[2]
+        if old_version
+          puts "Romove old version"
+          versions.elements.delete(old_version)
+        end
+
         build_folder = File.join(Dir.pwd, "build")
         preview_path = File.join(build_folder, "previews")
 
         current_version.elements.each("locales/locale") do |element| 
 
           element.elements.delete("app_previews")
-
+          element.elements.delete("software_screenshots")
+          
           locale = element.attributes["name"]
           locale_path = File.join(preview_path, locale)
-
+          # если нет папки с локалью, значит удаляем видео
+          app_previews = REXML::Element.new('app_previews')
           if File.directory?(locale_path)
             locale_files = Dir.entries(locale_path).select {|f| not File.directory?(f) }
-            app_previews = REXML::Element.new('app_previews')
 
             timestamp = "00:00:08:00"
             for file in locale_files do
@@ -55,51 +60,57 @@ module Fastlane
               if File.extname(file).downcase == ".mp4"
                 size = File.size(video_path)
                 if size > 500000
-
-                  file_with_locale = "#{locale}_#{file}"
-                  itmsp_video_path = File.join(itmsp_path, file_with_locale)
-                  FileUtils.cp(video_path, itmsp_video_path)
-                  file_name = File.basename(file, File.extname(file))
-
-                  app_preview = REXML::Element.new('app_preview')
-                  app_preview.add_attribute(REXML::Attribute.new('display_target', file_name))
-                  app_preview.add_attribute(REXML::Attribute.new('position', '1'))
-
-                  data_file = REXML::Element.new('data_file')
-                  data_file.add_attribute(REXML::Attribute.new('role', 'source'))
-
-                  size_node = REXML::Element.new('size')
-                  size_node.add_text("#{size}")
-                  data_file.elements.add(size_node)
-
-                  file_name_node = REXML::Element.new('file_name')
-                  file_name_node.add_text(file_with_locale)
-                  data_file.elements.add(file_name_node)
-
-                  checksum = REXML::Element.new('checksum')
-                  checksum.add_text(Digest::MD5.file(video_path).hexdigest)
-                  data_file.elements.add(checksum)
-
-                  app_preview.elements.add(data_file)
-
-                  preview_image_time = REXML::Element.new('preview_image_time')
-                  preview_image_time.add_attribute(REXML::Attribute.new('format', "30/1:1/nonDrop"))
-                  preview_image_time.add_text(timestamp)
-                  app_preview.elements.add(preview_image_time)
-
+                  app_preview = generate_app_preview(itmsp_path, locale, video_path, timestamp)
                   app_previews.elements.add(app_preview)
                 else
                   puts "#{video_path} too small, #{size}b"
                 end
               end
             end
-            element.elements.add(app_previews)
           end
+          element.elements.add(app_previews)
         end  
         formatter = REXML::Formatters::Pretty.new
         formatter.compact = true
         formatter.width = 9999
         File.open(metadata_path,"w"){|file| file.puts formatter.write(doc.root,"")}
+      end
+
+      def self.generate_app_preview(itmsp_path, locale, video_path, timestamp)
+        file = last = File.basename(video_path)
+        file_with_locale = "#{locale}_#{file}"
+        itmsp_video_path = File.join(itmsp_path, file_with_locale)
+        FileUtils.cp(video_path, itmsp_video_path)
+        file_name = File.basename(file, File.extname(file))
+
+        app_preview = REXML::Element.new('app_preview')
+        app_preview.add_attribute(REXML::Attribute.new('display_target', file_name))
+        app_preview.add_attribute(REXML::Attribute.new('position', '1'))
+
+        data_file = REXML::Element.new('data_file')
+        data_file.add_attribute(REXML::Attribute.new('role', 'source'))
+
+        size_node = REXML::Element.new('size')
+        size = File.size(video_path)
+        size_node.add_text("#{size}")
+        data_file.elements.add(size_node)
+
+        file_name_node = REXML::Element.new('file_name')
+        file_name_node.add_text(file_with_locale)
+        data_file.elements.add(file_name_node)
+
+        checksum = REXML::Element.new('checksum')
+        checksum.add_text(Digest::MD5.file(video_path).hexdigest)
+        data_file.elements.add(checksum)
+
+        app_preview.elements.add(data_file)
+
+        preview_image_time = REXML::Element.new('preview_image_time')
+        preview_image_time.add_attribute(REXML::Attribute.new('format', "30/1:1/nonDrop"))
+        preview_image_time.add_text(timestamp)
+        app_preview.elements.add(preview_image_time)
+
+        app_preview
       end
 
       #####################################################
